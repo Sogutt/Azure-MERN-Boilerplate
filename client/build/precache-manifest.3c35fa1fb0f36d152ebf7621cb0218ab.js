282d637a4fbde2f44fa9a4077853b53790772a61
Tuna Sogut <tuna.sogut@protonmail.com>
2019-12-29 19:00:03 -0500

    build fix

---
+++ client/build/precache-manifest.3c35fa1fb0f36d152ebf7621cb0218ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "207659dac292b7f616f14e00d9d51b7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80820ac3ebccad0f36d8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "401e745065819688c86e",
+    "url": "/static/js/2.e7b1de21.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e7b1de21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80820ac3ebccad0f36d8",
+    "url": "/static/js/main.d7b9e158.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
